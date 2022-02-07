f655f569c5d38dad434518ca829bbc3a784aaa0b
AntoXa PRO <info@antoxa.pro>
2022-02-07 21:26:48 +0300

    Add auth account form

---
+++ src/auth/forms/account.ts
@@ -0,0 +1,50 @@
+import { SchemaOf, object, boolean, string, array } from 'yup'
+import { BaseDbValidEntity } from '../../base'
+
+/**
+ * Тип данных (форма аккаунта)
+ */
+export type TAccountForm = {
+  disabled: boolean
+  email: string
+  groups: string[]
+
+  newPassword?: string
+}
+
+/**
+ * Схема валидации.
+ */
+export const accountValidSchema = {
+  disabled: boolean()
+    .meta({ label: 'Заблокирован' })
+    .default(false),
+  email: string()
+    .meta({ label: 'Email' })
+    .required('Укажите адрес электронной почты')
+    .email('Укажите корректный адрес электронной почты'),
+  groups: array()
+    .meta({ label: 'Группы' }),
+  newPassword: string()
+    .meta({ label: 'Новый пароль' })
+    .min(6, 'Пароль должен содержать минимум 6 символов')
+}
+
+/**
+ * Схема валидации формы.
+ */
+const accountFormValidSchema: SchemaOf<TAccountForm> = object(accountValidSchema)
+
+/**
+ * Форма аккаунта.
+ */
+export class AccountFormModel extends BaseDbValidEntity<TAccountForm> {
+  constructor(obj: any = {}) {
+    super(obj, accountFormValidSchema)
+  }
+
+  convertPreSave() {
+    this.obj.disabled = this.obj.disabled ? true : false
+    return this.obj
+  }
+}
--- src/auth/forms/index.ts
+++ src/auth/forms/index.ts
@@ -1,6 +1,7 @@
 import * as auth from './auth'
 import * as person from './person'
+import * as account from './account'
 import * as register from './register'
 import * as doc from './doc'
 
-export { auth, person, register, doc }
+export { auth, person, account, register, doc }
